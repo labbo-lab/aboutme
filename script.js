da88923a5c737d4e3f0bcf7a4597abe28642ac25
Vixey <62490883+labbo-lab@users.noreply.github.com>
2024-09-21 14:57:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 collapsables = document.querySelectorAll(".collapsable:not(.normal)")
 links = document.getElementsByClassName("url")
+scrolling = false
+
+document.onScroll = (e) => {
+  scrolling = true
+}
+
+document.onScrollEnd = (e) => {
+  scrolling = false
+}
 
 for (collapsable of collapsables) {
-  collapsable.setAttribute("onclick", "event.preventDefault();this.open = this.open ? false : true")
+  collapsable.setAttribute("onclick", "event.preventDefault();if(!scrolling){this.open = this.open ? false : true}")
 }
 
 for (link of links) {
